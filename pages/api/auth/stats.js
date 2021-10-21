148bfe56902a7525210d2323e2a05a8448ab7862
tun <bt5100@leto.eonet.ne.jp>
2021-10-21 18:25:37 +0900

    export default 下に移動

--- pages/api/auth/stats.js
+++ pages/api/auth/stats.js
@@ -1,9 +1,11 @@
 import{withPageAuthRequired}from '@auth0/nextjs-auth0';
 
-export default withPageAuthRequired(function ProtectedRoute(req, res){
+function ProtectedRoute(req, res){
 
 const session = getSession(req,res);
 
 const user = session.user;
 
-});
+};
+
+export default withPageAuthRequired(ProtectedRoute);
