fcd385daba1c05b299157dad3c3f8b85dc561f56
tun <bt5100@leto.eonet.ne.jp>
2021-10-21 09:45:00 +0900

    hooks

--- pages/userpage.js
+++ pages/userpage.js
@@ -4,16 +4,23 @@ import useSWR from 'swr'
 import {React} from 'react'
 import { useUser } from '@auth0/nextjs-auth0';
 
+const Getfetch = (user) => {
+
+  const fetcher = () => fetch('http://localhost:3001/user_title/' + user.sub).then(response => response.json());
+  return { data, error } = useSWR('http://localhost:3001/user_title/' + user.sub, fetcher);
+}
+
+const Getuser = () => {
+  return useUser();
+}
 
 export default function userpage() {
 
-  const { user, isLoading } = useUser();
+  const { user, isLoading } = Getuser();
 
   if (isLoading) return <div>Loading...</div>;
 
-  const fetcher = () => fetch('https://kakkowarai.herokuapp.com/user_title/' + user.sub).then(response => response.json());
-  
-  const { data, error } = useSWR('https://kakkowarai.herokuapp.com/user_title/' + user.sub, fetcher);
+  const { data, error } = Getfetch(user);  
     
   if (error) return <div>failed to load</div>
   if (!data) return <div>loading...</div>
