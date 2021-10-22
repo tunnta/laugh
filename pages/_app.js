43a1a80701e59109a102ee88893878b2c6b71e4a
tun <bt5100@leto.eonet.ne.jp>
2021-10-22 14:44:17 +0900

    theme

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,23 @@ import PropTypes from 'prop-types';
 import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Theme from './theme';
+
 import { UserProvider } from '@auth0/nextjs-auth0';
+import { createTheme } from '@material-ui/core/styles';
 
 function MyApp(props) {
   const { Component, pageProps } = props;
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#556cd6',
+      },
+      secondary: {
+        main: '#19857b',
+      },
+    },
+  });
 
   React.useEffect(() => {
     // Remove the server-side injected CSS.
@@ -23,7 +35,7 @@ function MyApp(props) {
         <title>My page</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
-      <ThemeProvider theme = {Theme()} >
+      <ThemeProvider theme = {theme} >
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <UserProvider>
