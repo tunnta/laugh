9adf3e2d83e6fd73b9409a13953db7bdd8b2e9f1
tun <bt5100@leto.eonet.ne.jp>
2021-10-23 15:19:23 +0900

    stats修正

--- pages/api/auth/stats.js
+++ pages/api/auth/stats.js
@@ -1,11 +1,10 @@
 import{withPageAuthRequired}from '@auth0/nextjs-auth0';
 
-function ProtectedRoute(req, res){
+export default withPageAuthRequired(function ProtectedRoute(req, res){
 
 const session = getSession(req,res);
 
 const user = session.user;
 
-};
+});
 
-export default withPageAuthRequired(ProtectedRoute);
