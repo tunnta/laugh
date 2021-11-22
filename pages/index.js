455a29206485a0f712c588ae0d8275345e56cef3
tun <bt5100@leto.eonet.ne.jp>
2021-11-23 00:44:52 +0900

    switch0のcontentを追加

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import Slide from '../components/slide'
 import Header from '../components/header'
-
-import AutoGrid from '../components/box2'
+import PastContent from '../components/past-content'
+import AutoGrid from '../components/current-content'
 import InteractiveList from '../components/ranklist'
 
 export async function getServerSideProps() {
   // posts を取得するため外部 API endpoint を読み込む
   const res =  await fetch('https://kakkowarai.herokuapp.com/title')
   const posts = await res.json()
-
+  const res2 =  await fetch('http://localhost:3001/title_content0')
+  const posts2 = await res2.json()
   // { props: { posts } }を返すことで、
   // Blog コンポーネントはビルド時に`posts`を prop として受け取る
   return {
@@ -30,7 +31,7 @@ function Index({posts}) {
     <InteractiveList/>
 
     <AutoGrid title = {posts}/>
-   
+    <PastContent title = {posts2}/>
         
     </div>
 
