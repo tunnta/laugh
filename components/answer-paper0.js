c5743f073127c0d586cfee4a869152a137e493cd
tun <bt5100@leto.eonet.ne.jp>
2021-12-07 22:56:52 +0900

    123位づけ

--- components/answer-paper0.js
+++ components/answer-paper0.js
@@ -7,27 +7,30 @@ import React from 'react'
 export default function AnswerPaper0(props) {
 
     const paperstyle = {'position':'relative',height:'40vh',width:820,padding:90,fontSize:30,'backgroundImage':'url(/f1391_2.png)',"backgroundRepeat":"noRepeat",'backgroundSize':'100% 100%',"backgroundPosition":"center center",'textAlign': 'center'}
+    const paperstyle1 = {'position':'relative',height:'40vh',width:820,padding:90,fontSize:30,'backgroundImage':'url(/f0919_5.png)',"backgroundRepeat":"noRepeat",'backgroundSize':'100% 100%',"backgroundPosition":"center center",'textAlign': 'center'}
+    const paperstyle2 = {'position':'relative',height:'40vh',width:820,padding:90,fontSize:30,'backgroundImage':'url(/f0919_52.png)',"backgroundRepeat":"noRepeat",'backgroundSize':'100% 100%',"backgroundPosition":"center center",'textAlign': 'center'}
+    const paperstyle3 = {'position':'relative',height:'40vh',width:820,padding:90,fontSize:30,'backgroundImage':'url(/f0919_53.png)',"backgroundRepeat":"noRepeat",'backgroundSize':'100% 100%',"backgroundPosition":"center center",'textAlign': 'center'}
     const goodstyle = {'position':'absolute',bottom: 0,marginBottom:20}
     const array = props.data.slice( 3 );
 
     if (props.data.length > 3) return (
       <div> 
 
-        <Paper elevation={0} style = {paperstyle}>{props.data[0].answer}
+        <Paper elevation={0} style = {paperstyle1}>{props.data[0].answer}
           <div style = {goodstyle}>       
             <RecommendIcon/>
             {props.data[0].count}
           </div>
         </Paper>
 
-        <Paper elevation={0} style = {paperstyle}>{props.data[1].answer}
+        <Paper elevation={0} style = {paperstyle2}>{props.data[1].answer}
           <div style = {goodstyle}>       
             <RecommendIcon/>
             {props.data[1].count}
           </div>
         </Paper>
 
-        <Paper elevation={0} style = {paperstyle}>{props.data[2].answer}
+        <Paper elevation={0} style = {paperstyle3}>{props.data[2].answer}
           <div style = {goodstyle}>       
             <RecommendIcon/>
             {props.data[2].count}
@@ -49,7 +52,7 @@ if (props.data.length === 1) return (
 
      <div> 
 
-        <Paper elevation={0} style = {paperstyle}>{props.data[0].answer}
+        <Paper elevation={0} style = {paperstyle1}>{props.data[0].answer}
           <div style = {goodstyle}>           
             <RecommendIcon/>
             {props.data[0].count}
@@ -63,14 +66,14 @@ if (props.data.length === 2) return (
 
         <div> 
 
-           <Paper elevation={0} style = {paperstyle}>{props.data[0].answer}
+           <Paper elevation={0} style = {paperstyle1}>{props.data[0].answer}
              <div style = {goodstyle}>
                 <RecommendIcon/>
                 {props.data[0].count}
              </div>
            </Paper>
 
-           <Paper elevation={0} style = {paperstyle}>{props.data[1].answer}
+           <Paper elevation={0} style = {paperstyle2}>{props.data[1].answer}
              <div style = {goodstyle}>
                 <RecommendIcon/>
                 {props.data[1].count}
@@ -84,21 +87,21 @@ if (props.data.length === 2) return (
 if (props.data.length === 3) return (
         <div > 
 
-           <Paper elevation={0} style = {paperstyle}>{props.data[0].answer} 
+           <Paper elevation={0} style = {paperstyle1}>{props.data[0].answer} 
               <div style = {goodstyle}>
                 <RecommendIcon style = {{color:"black"}}/>
                 {props.data[0].count}
               </div>
             </Paper>
 
-            <Paper elevation={0} style = {paperstyle}>{props.data[1].answer}
+            <Paper elevation={0} style = {paperstyle2}>{props.data[1].answer}
               <div style = {goodstyle}>
                 <RecommendIcon/>
                 {props.data[1].count}
               </div>
            </Paper>
 
-           <Paper elevation={0} style = {paperstyle}>{props.data[2].answer}
+           <Paper elevation={0} style = {paperstyle3}>{props.data[2].answer}
                <div style = {goodstyle}>
                  <RecommendIcon />
                  {props.data[2].count}
