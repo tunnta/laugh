669fcdd6133902c324cc0f7c90be8571d73d4e3e
tun <bt5100@leto.eonet.ne.jp>
2022-01-06 17:11:39 +0900

    css調整

--- components/header.js
+++ components/header.js
@@ -7,111 +7,106 @@ function Header(){
   const { user, loading } = useUser();
 
   return (
-    <header>
-      <nav>
-        <ul>
-          
-          {!loading &&
-            (user ? (
-              <>
-                <li>
-                <Link href="/userpage">
-                  <a>ユーザー</a>
-                </Link>
-                </li>{' '}
-                <li>
-                <Link href="/api/auth/logout">
-                  <a>ログアウト</a>
-                </Link>
-                </li>
-              </>
-            ) : (
-              <>
-                <li>
-                <Link href="/api/auth/login">
-                  <a>ログイン</a>
-                </Link>
-                </li>
-              </>
-            ))}
-        </ul>
-      </nav>
+    <>
       <MediaQuery minWidth={415}>
-      <style jsx>{`
-        header {
-          padding: 0.05rem;
-          color: #fff;
-          background-color: #333;
-          min-width:1200px;
-        }
-        nav {
-          max-width: 42rem;
-          margin: 1.2rem auto;
-          margin-left: 80%;
-        }
-        ul {
-          display: flex;
-          list-style: none;
-          margin-left: 0;
-          padding-left: 0;
-        }
-        li {
-          margin-right: 1rem;
-        }
-        li:nth-child(3) {
-          margin-right: auto;
-        }
-        a {
-          color: #fff;
-          text-decoration: none;
-        }
-        button {
-          font-size: 1rem;
-          color: #fff;
-          cursor: pointer;
-          border: none;
-          background: none;
-        }
-      `}</style>
+        <header style ={{'min-width':'1200px'}}>
+          <nav>
+            <ul>
+              {!loading &&
+                (user ? (
+                  <>
+                    <li>
+                    <Link href="/userpage">
+                      <a>ユーザー</a>
+                    </Link>
+                    </li>{' '}
+                    <li>
+                    <Link href="/api/auth/logout">
+                      <a>ログアウト</a>
+                    </Link>
+                    </li>
+                  </>
+                ) : (
+                  <>
+                    <li>
+                    <Link href="/api/auth/login">
+                      <a>ログイン</a>
+                    </Link>
+                    </li>
+                  </>
+                ))}
+            </ul>
+          </nav>
+        </header>
       </MediaQuery>
       <MediaQuery maxWidth={414}>
-      <style jsx>{`
-        header {
-          padding: 0.05rem;
-          color: #fff;
-          background-color: #333;
-        }
-        nav {
-          max-width: 42rem;
-          margin: 1.2rem auto;
-          margin-left: 80%;
-        }
-        ul {
-          display: flex;
-          list-style: none;
-          margin-left: 0;
-          padding-left: 0;
-        }
-        li {
-          margin-right: 1rem;
-        }
-        li:nth-child(3) {
-          margin-right: auto;
-        }
-        a {
-          color: #fff;
-          text-decoration: none;
-        }
-        button {
-          font-size: 1rem;
-          color: #fff;
-          cursor: pointer;
-          border: none;
-          background: none;
-        }
-      `}</style>
+        <header>
+          <nav>
+            <ul>
+              {!loading &&
+                (user ? (
+                  <>
+                    <li>
+                    <Link href="/userpage">
+                      <a>ユーザー</a>
+                    </Link>
+                    </li>{' '}
+                    <li>
+                    <Link href="/api/auth/logout">
+                      <a>ログアウト</a>
+                    </Link>
+                    </li>
+                  </>
+                ) : (
+                  <>
+                    <li>
+                    <Link href="/api/auth/login">
+                      <a>ログイン</a>
+                    </Link>
+                    </li>
+                  </>
+                ))}
+            </ul>
+          </nav>
+        </header>
       </MediaQuery>
-    </header>
+      <style jsx>{`
+          header {
+            padding: 0.05rem;
+            color: #fff;
+            background-color: #333;
+            min-width:1200px;
+          }
+          nav {
+            max-width: 42rem;
+            margin: 1.2rem auto;
+            margin-left: 80%;
+          }
+          ul {
+            display: flex;
+            list-style: none;
+            margin-left: 0;
+            padding-left: 0;
+          }
+          li {
+            margin-right: 1rem;
+          }
+          li:nth-child(3) {
+            margin-right: auto;
+          }
+          a {
+            color: #fff;
+            text-decoration: none;
+          }
+          button {
+            font-size: 1rem;
+            color: #fff;
+            cursor: pointer;
+            border: none;
+            background: none;
+          }
+        `}</style>
+      </>
   );
 };
 
