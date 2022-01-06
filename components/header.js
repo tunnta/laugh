f9d71e50b92c1d6ef3abe81cc6e2468a986d0047
tun <bt5100@leto.eonet.ne.jp>
2022-01-06 22:56:52 +0900

    css調整

--- components/header.js
+++ components/header.js
@@ -80,7 +80,7 @@ function Header(){
           nav {
             max-width: 42rem;
             margin: 1.2rem auto;
-            right: 0
+            margin: 0 0 0 auto;
           }
           ul {
             display: flex;
